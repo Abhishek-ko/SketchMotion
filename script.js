2331863ce2172566baad2f7be3f3e8cb5debf0a9
Abhishek kumar <abhishekch2906@gmail.com>
2026-03-27 20:42:48 +0530

    Added frame storage system

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const frameContainer = document.getElementById("frameContainer");
+const addFrameBtn = document.getElementById("addFrame");
+
 let drawing = false;
+
+let frames = [];
+let currentFrame = 0;
 
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", stopDrawing);
@@ -15,6 +21,8 @@ draw(e);
 function stopDrawing(){
 drawing = false;
 ctx.beginPath();
+
+saveFrame();
 }
 
 function draw(e){
@@ -27,7 +35,61 @@ ctx.strokeStyle = "black";
 
 ctx.lineTo(e.offsetX, e.offsetY);
 ctx.stroke();
+
 ctx.beginPath();
 ctx.moveTo(e.offsetX, e.offsetY);
 
 }
+
+function saveFrame(){
+
+frames[currentFrame] = canvas.toDataURL();
+
+}
+
+function loadFrame(index){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+let img = new Image();
+
+img.src = frames[index];
+
+img.onload = function(){
+ctx.drawImage(img,0,0);
+};
+
+}
+
+function createFrameButton(index){
+
+const btn = document.createElement("button");
+
+btn.innerText = index + 1;
+
+btn.onclick = function(){
+
+saveFrame();
+
+currentFrame = index;
+
+loadFrame(index);
+
+};
+
+frameContainer.appendChild(btn);
+
+}
+
+addFrameBtn.onclick = function(){
+
+saveFrame();
+
+frames.push(null);
+
+createFrameButton(frames.length - 1);
+
+};
+
+frames.push(null);
+createFrameButton(0);
